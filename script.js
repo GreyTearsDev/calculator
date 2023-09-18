0653d857ac798cc437555e6aa842918ecc4eed4f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-09-18 22:32:34 +0100

    Add switch statement to handle which function to call
    the correct function will be called based on the operation simbol used as an argument

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-function add() {
+let firstNumber, operator, secondNumber;
 
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 }
 
-function subtract() {
-    
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 }
 
-function multiply() {
-    
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
 }
 
-function divide() {
-    
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+  switch (operator) {
+    case "+":
+      add(firstNumber, secondNumber);
+      break;
+    case "-":
+      subtract(firstNumber, secondNumber);
+			break;
+		case "*":
+			multiply(firstNumber, secondNumber);
+			break;
+		case "/":
+			divide(firstNumber, secondNumber);
+			break;
+  }
 }
