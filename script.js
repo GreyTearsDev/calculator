3d5a132d0e7d4b4a36e42270bc6243affd58af65
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-09-20 00:00:00 +0100

    Add event listener to digit buttons
    now, whenever they get clicked on, their content is shown on the display

--- script.js
+++ script.js
@@ -23,14 +23,24 @@ function operate(operator, firstNumber, secondNumber) {
       break;
     case "-":
       subtract(firstNumber, secondNumber);
-			break;
-		case "*":
-			multiply(firstNumber, secondNumber);
-			break;
-		case "/":
-			divide(firstNumber, secondNumber);
-			break;
+      break;
+    case "*":
+      multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      divide(firstNumber, secondNumber);
+      break;
   }
 }
 
+let digitButtons = Array.from(document.querySelectorAll(".digit"));
+digitButtons.forEach((e) => e.addEventListener("click", (event) => displayDigitOnScreen(event)))
+// add event listener to the buttons
 
+
+
+function displayDigitOnScreen(event) {
+	let display = document.querySelector(".display");
+	let displayValue = event.target.textContent;
+  display.textContent += displayValue;
+}
