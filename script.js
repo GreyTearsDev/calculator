864edad41ed49faab30e2613d410f17101a0ed2f
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-09-29 19:19:41 +0100

    Create 'processInput' fucntion
    it stores the input numbers and operators in arrays '

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let firstNumber, operator, secondNumber;
+let displayValue = [];
+let operatorsToUse = []; 
+
 
 function add(firstNumber, secondNumber) {
   return firstNumber + secondNumber;
@@ -17,19 +20,15 @@ function divide(firstNumber, secondNumber) {
 }
 
 function operate(operator, firstNumber, secondNumber) {
-  switch (operator) {
-    case "+":
-      add(firstNumber, secondNumber);
-      break;
-    case "-":
-      subtract(firstNumber, secondNumber);
-      break;
-    case "*":
-      multiply(firstNumber, secondNumber);
-      break;
-    case "/":
-      divide(firstNumber, secondNumber);
-      break;
+  //
+  if (operator == '*') {
+    multiply(firstNumber, secondNumber)
+  } else if ( operator == '/') {
+    divide(firstNumber, secondNumber);
+  } else if (operator == '+') {
+    add(firstNumber, secondNumber);
+  } else if (operator == '-') {
+    subtract(firstNumber, secondNumber);
   }
 }
 
@@ -40,14 +39,40 @@ digitButtons.forEach((e) =>
 );
 // add listener for storing the digits
 digitButtons.forEach((e) =>
-  e.addEventListener("click", (event) => storeDigits(event))
+  e.addEventListener("click", (event) => processInput(event))
 );
 
 // Stores the value of the button that was clicked
-let displayValue = [];
-function storeDigits(event) {
-  displayValue.push(event.target.textContent);
+
+function processInput(event) {
+  //displayValue.push(event.target.textContent);
+  let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let operators = ['+', '-', '*', '/'];
+  let input = event.target.textContent;
+
+  if (digits.includes(input)) {
+    displayValue.push(input); 
+    console.log();
+  } else if (operators.includes(input)) {
+    // if the user enters an operator
+    if (firstNumber == null) {
+      operators.push(input); // store the operator
+      firstNumber = displayValue.join(''); // concatenate the strung numbers into a single string
+      displayValue.length = 0; // delete elements in the array
+    } else if (secondNumber == null) {
+      operators.push(input);
+      secondNumber = displayValue.join('');
+      displayValue.length = 0; // delete elements in the array
+    }
+  }
+  
+  console.log(displayValue);
+  console.log(displayValue.length);
+  console.log(firstNumber);
+  console.log(secondNumber);
 }
+
+
 
 // Displays the value of the clicked buttons on the screen
 function displayDigitOnScreen(event) {
@@ -56,6 +81,24 @@ function displayDigitOnScreen(event) {
   display.textContent += displayValueOnScreen; // adds the value of the clicked button to the screen
 }
 
-console.log(displayValue);
+
 
 //for every character in the display, if it is an operator,
+
+
+
+// add the numbers in an array as a long string
+
+
+
+// every time an operator is clicked, a blank space is added 
+// between the numbers.
+// every time an operator is clicked on, it is added to its 
+// own array of operators
+// split the string of numbers into individual array elements 
+// separated by the empty space
+
+
+//store the numbers 
+
+
