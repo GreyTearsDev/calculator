f663bd4b8b819c10c947e95b3431b56956e37d61
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2023-10-07 14:56:25 +0100

    Stop user from entering more than one decimal point per number

--- script.js
+++ script.js
@@ -54,52 +54,73 @@ function operate(operator, firstNumber, secondNumber) {
 function displayOnScreen(event) {
   let input = event.target.textContent;
   let displayIsFull = storedValues.length >= 18;
+  let decimalIsPresent = displayScreen.textContent.includes(".");
+  console.log(decimalIsPresent);
 
   if (input == "D") {
-    displayScreen.textContent = displayScreen.textContent.slice(0, -1);
+    displayScreen.textContent = displayScreen.textContent.slice(0, -1); // Delete the last element on the screen
   } else if (!displayIsFull) {
-    displayScreen.textContent += input;
+    if (decimalIsPresent) {
+      switch (input == ".") { // Check if the new input is a decimal point
+        case true:
+          break;
+        case false:
+          displayScreen.textContent += input; 
+      }
+    } else {
+      displayScreen.textContent += input;
+    }
   }
 }
 
 function saveNumber(event) {
   let input = event.target.textContent;
-  let ongoingCalculationExists = (operator === "");
+  let ongoingCalculationExists = operator === "";
   let storedNumber = parseFloat(storedValues.join(""));
+  
   let isDigit = /[\d.]/.test(input);
   let isOperator = /[+\-//x]/.test(input);
   let equalButtonClicked = /[=]/.test(input);
   let clearButtonClicked = /[C]/.test(input);
   let deleteButtonClicked = /[D]/.test(input);
+  let isDecimal = /[.]/.test(input);
 
   if (isDigit) {
-
-    if (storedValues.length < 18) {
+    if (isDecimal) {
+      switch (storedValues.includes(".")) {
+        case false:
+          storedValues.push(input);
+          break;
+        case true:
+          break;
+      }
+    } else if (storedValues.length < 18) {
       storedValues.push(input);
     }
   } else if (isOperator) {
     // Check if there is already an ongoing calculation
-    if (ongoingCalculationExists) { 
-       // Handle operators here, set operator variable
-       operator = input;
-       firstNumber = storedNumber
-       storedValues = []; // Clear storedValues for the next number
-    } else { // 
-      secondNumber = storedNumber
+    if (ongoingCalculationExists) {
+      // Handle operators here, set operator variable
+      operator = input;
+      firstNumber = storedNumber;
+      storedValues = []; // Clear storedValues for the next number
+    } else {
+      //
+      secondNumber = storedNumber;
       result = operate(operator, firstNumber, secondNumber);
       firstNumber = result;
       storedValues = []; // Clear storedValues after calculation
       secondNumber = "";
       operator = input;
-    } 
+    }
   } else if (equalButtonClicked) {
-    secondNumber = storedNumber
+    secondNumber = storedNumber;
     result = operate(operator, firstNumber, secondNumber);
     storedValues = []; // Clear storedValues after calculation
     storedValues.push(result); //Save the current result for later calculations
     displayScreen.textContent = result;
     firstNumber = result;
-    operator = '';
+    operator = "";
     secondNumber = "";
   } else if (clearButtonClicked) {
     // Clear the variables for new operations
